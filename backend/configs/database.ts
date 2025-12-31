dd012b4306256f6d0758017680e0d28dfbc45792
taufanrizkii <taufanrizkia1@gmail.com>
2025-12-31 22:12:03 +0700

    update hapus button di AppLayout.ts

--- backend/configs/database.ts
+++ backend/configs/database.ts
@@ -5,7 +5,7 @@ export const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "db_efilling",
-  password: "user", // GANTI DENGAN PASSWORD POSTGRES ANDA
+  password: "admin", // GANTI DENGAN PASSWORD POSTGRES ANDA
   port: 5432,
 });
 
--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -5,18 +5,13 @@
 import React, { useState } from 'react';
 import { usePathname } from "next/navigation";
 import Sidebar from "@/components/ui/Sidebar";
-import { Toaster } from "sonner";
+import { Toaster } from "sonner"; // Pastikan import ini ada
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  // State untuk Desktop Sidebar Minimize
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
   const pathname = usePathname();
-  
-  // Cek apakah url saat ini adalah halaman login
   const isLoginPage = pathname === "/login";
 
-  // Jika di halaman login, render konten tanpa Sidebar
   if (isLoginPage) {
     return (
       <>
@@ -26,16 +21,13 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     );
   }
 
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col md:flex-row">
-      
       <Sidebar 
         isOpen={isSidebarOpen} 
         onToggle={() => setIsSidebarOpen(!isSidebarOpen)} 
       />
 
-      {/* Main Content dengan Padding yang Disesuaikan */}
       <main 
         className={`
           flex-1 transition-all duration-300 ease-in-out p-6 w-full
@@ -44,6 +36,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       >
         {children}
       </main>
+
+      {/* TAMBAHKAN INI DI SINI AGAR NOTIFIKASI MUNCUL */}
+      <Toaster richColors position="top-center" />
     </div>
   );
 }
